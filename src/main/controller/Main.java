9a3bc9ee97a30ab024fdf7041e710bc03fcfd06e
gyu9907 <gyu9907@gmail.com>
2024-10-22 14:12:18 +0900

    chore: 경고메시지 출력 메소드 사용

--- src/main/controller/Main.java
+++ src/main/controller/Main.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 import common.ProjectDBConnection;
 import member.controller.MemberController;
+import utils.Msg;
 
 public class Main {
 
@@ -11,16 +12,17 @@ public class Main {
 		
 		MemberController ctrl = new MemberController();
 		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("=====< 메인메뉴 >=====\n"
-				+ "1. 회원가입   2. 로그인\n"
-				+ "0. 프로그램 종료\n"
-				+ "======================");
-		
-		System.out.print("▷ 메뉴 번호 선택 : ");
-		String menu = sc.nextLine();
+		String menu;
 		
 		do {
+			System.out.println("\n=====< 메인메뉴 >=====\n"
+					+ "1. 회원가입   2. 로그인\n"
+					+ "0. 프로그램 종료\n"
+					+ "======================");
+			
+			System.out.print("▷ 메뉴 번호 선택 : ");
+			menu = sc.nextLine();
+			
 			switch (menu) {
 				case "1": // 회원가입
 					ctrl.register(sc);
@@ -35,7 +37,7 @@ public class Main {
 					break;
 		
 				default:
-					System.out.println(">> [경고] 입력하신 메뉴 번호 "+menu+"는 존재하지 않습니다. <<");
+					Msg.W("입력하신 메뉴 번호 " + menu + "는 존재하지 않습니다.");
 					break;
 			}
 		} while(!"0".equals(menu));
