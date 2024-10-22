07901cdeb57d34601494606bc1384e6390e3afba
js813N <gpflvhxj113@naver.com>
2024-10-22 16:24:08 +0900

    feat : 관리자 로그인 변경, 관리자 전용메뉴 기능 추가
    1. 관리자 로그인에서 LoginDTO를 삭제하고 HashMap 사용
    2. 관리자 전용메뉴 기능 추가
    3. AdminDTO에서 admin_id -> adminId로 변경

--- src/admin/model/AdminDAO.java
+++ src/admin/model/AdminDAO.java
@@ -1,12 +1,15 @@
 package admin.model;
 
-import admin.domain.AdminDTO;
-import admin.domain.AdminLoginDTO;
+import java.util.Map;
 
+import admin.domain.AdminDTO;
+/*
+ * 관리자와 관련된 DB 데이터 접근 인터페이스
+ */
 public interface AdminDAO {
 
 	/*
 	 * 관리자 로그인
 	 */
-	AdminDTO login(AdminLoginDTO loginDTO);
+	AdminDTO login(Map<String, String> loginMap);
 }
