f24ee9b7b147c72999051ed71035cf7d19657141
Kang-Yi-Hun <rkddlgns1110@gmail.com>
2024-10-23 14:40:23 +0900

    feat : 입사지원에 관한 DAO, DTO

--- src/apply/model/ApplyDAO.java
+++ src/apply/model/ApplyDAO.java
@@ -1,5 +1,13 @@
 package apply.model;
+
+import java.util.List;
+
+import apply.domain.ApplyDTO;
+
 //입사지원서
 public interface ApplyDAO {
+
+	// 입자지원자 목록
+	List<ApplyDTO> applyList();
 
 }
--- src/apply/model/ApplyDAO_imple.java
+++ src/apply/model/ApplyDAO_imple.java
@@ -1,5 +1,53 @@
 package apply.model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import apply.domain.ApplyDTO;
+import common.ProjectDBConnection;
+
 public class ApplyDAO_imple implements ApplyDAO {
+
+	// field, attribute, property, 속성
+	private Connection conn = ProjectDBConnection.getConn();
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+
+	
+	
+	// method, operation, 기능
+	
+	// === 자원반납을 해주는 메소드 === //
+	private void close() { // 조회, 삽입, 수정, 삭제마다 전부 넣으면 너무 길어지고 쓸데없으니 메소드화 해서 한줄로 끝내자!
+		try {
+			if(rs != null) { rs.close(); rs = null;} // 확인사살
+			if(pstmt != null) { pstmt.close(); pstmt = null; }
+		} catch (SQLException e) {e.printStackTrace();}
+	} // end of private void close()------------
+	
+	
+	
+	// === 입자지원자 목록을 보여주는 메소드 === //
+	@Override
+	public List<ApplyDTO> applyList() {
+
+		List<ApplyDTO> ApplyList = new ArrayList<>();
+		
+		try {
+			
+			String sql = "  ";
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		
+		return null;
+	}
 
 }
