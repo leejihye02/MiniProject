a662e9b1e1bf38ad12cfb1f56bc8e588cc3eeb9f
js813N <gpflvhxj113@naver.com>
2024-10-24 10:54:11 +0900

    chore : ReviewAuthDAO_imple 코드 정렬

--- src/review/model/ReviewAuthDAO_imple.java
+++ src/review/model/ReviewAuthDAO_imple.java
@@ -23,39 +23,38 @@ public class ReviewAuthDAO_imple implements ReviewAuthDAO {
 	// === 자원반납 === //
 	private void close() {
 		try {
-			if (rs != null) {
-				rs.close();
-				rs = null;
-			}
-			if (pstmt != null) {
-				pstmt.close();
-				pstmt = null;
-			}
+			if(rs != null) { rs.close(); rs = null; }
+			if(pstmt != null) { pstmt.close(); pstmt = null; }
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}
+		}	
 	}// end of close()-------------------------
-
+	
+	
 	// === 회사인증목록 select === //
 	@Override
 	public List<Map<String, String>> getReviewAuth(String applicantId) {
 		List<Map<String, String>> mapList = new ArrayList<>();
-
+		
 		try {
 			String sql = " select review_auth_id, fk_applicant_id, fk_company_id, name "
-					+ "      , decode(is_permitted, 0, '요청중', 1, '완료', '거부됨') AS is_permitted "
-					+ "      , decode((select count(*) " + "         from tbl_review "
-					+ "         where fk_applicant_id = ? and fk_company_id = RA.fk_company_id "
-					+ "         ), 0, '새로작성하기', '작성됨') as is_registerd " + " from tbl_review_auth RA "
-					+ " JOIN tbl_company C " + " ON RA.fk_company_id = C.company_id " + " where fk_applicant_id = ? ";
-
+					   + "      , decode(is_permitted, 0, '요청중', 1, '완료', '거부됨') AS is_permitted "
+					   + "      , decode((select count(*) "
+					   + "         from tbl_review "
+					   + "         where fk_applicant_id = ? and fk_company_id = RA.fk_company_id "
+					   + "         ), 0, '새로작성하기', '작성됨') as is_registerd "
+					   + " from tbl_review_auth RA "
+					   + " JOIN tbl_company C "
+					   + " ON RA.fk_company_id = C.company_id "
+					   + " where fk_applicant_id = ? ";
+			
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, applicantId);
 			pstmt.setString(2, applicantId);
-
+			
 			rs = pstmt.executeQuery(); // sql문 실행
-
-			while (rs.next()) {
+			
+			while(rs.next()) {
 				Map<String, String> reviewAuthMap = new HashMap<>();
 
 				reviewAuthMap.put("review_auth_id", rs.getString("review_auth_id"));
@@ -64,42 +63,44 @@ public class ReviewAuthDAO_imple implements ReviewAuthDAO {
 				reviewAuthMap.put("company_name", rs.getString("name"));
 				reviewAuthMap.put("is_permitted", rs.getString("is_permitted"));
 				reviewAuthMap.put("is_registerd", rs.getString("is_registerd"));
-
+				
 				mapList.add(reviewAuthMap);
 			}
-
-		} catch (SQLException e) {
+			
+		} catch(SQLException e) {
 			e.printStackTrace();
 		} finally {
 			close();
 		}
-
+		
 		return mapList;
 	}
 
+	
 	// === 회사인증 insert === //
 	@Override
 	public int registerReviewAuth(ReviewAuthDTO reviewAuthDTO) {
 		int result = 0;
-
+		
 		try {
 			String sql = " insert into tbl_review_auth(review_auth_id, fk_applicant_id, fk_company_id) "
-					+ " values(seq_review_auth_id.nextval, ?, ?) ";
-
+					   + " values(seq_review_auth_id.nextval, ?, ?) ";
+			
 			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, reviewAuthDTO.getFkApplicantId());
-			pstmt.setString(2, reviewAuthDTO.getFkCompanyId());
-
+			pstmt.setString(1,  reviewAuthDTO.getFkApplicantId());
+			pstmt.setString(2,  reviewAuthDTO.getFkCompanyId());
+			
 			result = pstmt.executeUpdate(); // sql문 실행
-
-		} catch (SQLException e) {
+			
+		} catch(SQLException e) {
 			e.printStackTrace();
 		} finally {
 			close();
 		}
-
+		
 		return result;
 	}
+
 
 	// === 회사인증 요청 목록 조회 === //
 	@Override
