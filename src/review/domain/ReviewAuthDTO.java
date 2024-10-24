7e021801d74471ac69ab40bf0ad8b8818ca8ed37
js813N <gpflvhxj113@naver.com>
2024-10-24 10:48:19 +0900

    feat : 회사인증 요청 조회, 허가 기능 추가
    1. 구직자의 회사인증 요청 목록을 조회하고 허가 또는 거부처리 기능 추가

--- src/review/domain/ReviewAuthDTO.java
+++ src/review/domain/ReviewAuthDTO.java
@@ -7,6 +7,10 @@ public class ReviewAuthDTO {
     private String fkApplicantId; // 구직자아이디
     private String fkCompanyId;   // 회사아이디
     private int isPermitted;      // 관리자허가여부 0:요청 1:허용 2:거부
+    
+    // 조회용
+    private String applicantName; // 구직자 성명
+    private String companyName; // 회사명
 
 	// method
 	public int getReviewAuthId() {
@@ -39,5 +43,21 @@ public class ReviewAuthDTO {
 	
 	public void setIsPermitted(int isPermitted) {
 		this.isPermitted = isPermitted;
+	}
+	
+	public String getApplicantName() {
+		return applicantName;
+	}
+
+	public void setApplicantName(String applicantName) {
+		this.applicantName = applicantName;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
 	}
 }
--- src/review/model/ReviewAuthDAO_imple.java
+++ src/review/model/ReviewAuthDAO_imple.java
@@ -18,44 +18,44 @@ public class ReviewAuthDAO_imple implements ReviewAuthDAO {
 	private Connection conn = ProjectDBConnection.getConn();
 	private PreparedStatement pstmt;
 	private ResultSet rs;
-	
-	
+
 	// method
 	// === 자원반납 === //
 	private void close() {
 		try {
-			if(rs != null) { rs.close(); rs = null; }
-			if(pstmt != null) { pstmt.close(); pstmt = null; }
+			if (rs != null) {
+				rs.close();
+				rs = null;
+			}
+			if (pstmt != null) {
+				pstmt.close();
+				pstmt = null;
+			}
 		} catch (SQLException e) {
 			e.printStackTrace();
-		}	
+		}
 	}// end of close()-------------------------
-	
-	
+
 	// === 회사인증목록 select === //
 	@Override
 	public List<Map<String, String>> getReviewAuth(String applicantId) {
 		List<Map<String, String>> mapList = new ArrayList<>();
-		
+
 		try {
 			String sql = " select review_auth_id, fk_applicant_id, fk_company_id, name "
-					   + "      , decode(is_permitted, 0, '요청중', 1, '완료', '거부됨') AS is_permitted "
-					   + "      , decode((select count(*) "
-					   + "         from tbl_review "
-					   + "         where fk_applicant_id = ? and fk_company_id = RA.fk_company_id "
-					   + "         ), 0, '새로작성하기', '작성됨') as is_registerd "
-					   + " from tbl_review_auth RA "
-					   + " JOIN tbl_company C "
-					   + " ON RA.fk_company_id = C.company_id "
-					   + " where fk_applicant_id = ? ";
-			
+					+ "      , decode(is_permitted, 0, '요청중', 1, '완료', '거부됨') AS is_permitted "
+					+ "      , decode((select count(*) " + "         from tbl_review "
+					+ "         where fk_applicant_id = ? and fk_company_id = RA.fk_company_id "
+					+ "         ), 0, '새로작성하기', '작성됨') as is_registerd " + " from tbl_review_auth RA "
+					+ " JOIN tbl_company C " + " ON RA.fk_company_id = C.company_id " + " where fk_applicant_id = ? ";
+
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, applicantId);
 			pstmt.setString(2, applicantId);
-			
+
 			rs = pstmt.executeQuery(); // sql문 실행
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				Map<String, String> reviewAuthMap = new HashMap<>();
 
 				reviewAuthMap.put("review_auth_id", rs.getString("review_auth_id"));
@@ -64,42 +64,105 @@ public class ReviewAuthDAO_imple implements ReviewAuthDAO {
 				reviewAuthMap.put("company_name", rs.getString("name"));
 				reviewAuthMap.put("is_permitted", rs.getString("is_permitted"));
 				reviewAuthMap.put("is_registerd", rs.getString("is_registerd"));
-				
+
 				mapList.add(reviewAuthMap);
 			}
-			
-		} catch(SQLException e) {
+
+		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
 			close();
 		}
-		
+
 		return mapList;
 	}
 
-	
 	// === 회사인증 insert === //
 	@Override
 	public int registerReviewAuth(ReviewAuthDTO reviewAuthDTO) {
 		int result = 0;
-		
+
 		try {
 			String sql = " insert into tbl_review_auth(review_auth_id, fk_applicant_id, fk_company_id) "
-					   + " values(seq_review_auth_id.nextval, ?, ?) ";
-			
+					+ " values(seq_review_auth_id.nextval, ?, ?) ";
+
 			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1,  reviewAuthDTO.getFkApplicantId());
-			pstmt.setString(2,  reviewAuthDTO.getFkCompanyId());
-			
+			pstmt.setString(1, reviewAuthDTO.getFkApplicantId());
+			pstmt.setString(2, reviewAuthDTO.getFkCompanyId());
+
 			result = pstmt.executeUpdate(); // sql문 실행
-			
-		} catch(SQLException e) {
+
+		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
 			close();
 		}
-		
+
 		return result;
+	}
+
+	// === 회사인증 요청 목록 조회 === //
+	@Override
+	public List<ReviewAuthDTO> getReqReviewAuthList() {
+		List<ReviewAuthDTO> reviewAuthList = new ArrayList<>();
+		ReviewAuthDTO reviewAuthDTO = null;
+
+		try {
+			String sql = " select r.review_auth_id, a.name as applicant_name, r.fk_applicant_id, c.name as company_name, r.fk_company_id "
+					+ " from tbl_review_auth r join tbl_applicant a on r.fk_applicant_id = a.applicant_id "
+					+ " join tbl_company c on r.fk_company_id = c.company_id " + " where is_permitted = 0 ";
+
+			pstmt = conn.prepareStatement(sql);
+
+			rs = pstmt.executeQuery(); // sql문 실행
+
+			while (rs.next()) {
+
+				reviewAuthDTO = new ReviewAuthDTO();
+
+				reviewAuthDTO.setReviewAuthId(rs.getInt("review_auth_id"));
+				reviewAuthDTO.setApplicantName(rs.getString("applicant_name"));
+				reviewAuthDTO.setFkApplicantId(rs.getString("fk_applicant_id"));
+				reviewAuthDTO.setCompanyName(rs.getString("company_name"));
+				reviewAuthDTO.setFkCompanyId(rs.getString("fk_company_id"));
+
+				reviewAuthList.add(reviewAuthDTO);
+
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+
+		return reviewAuthList;
+	}
+
+	
+	// === 회사인증 요청 허가/거부 === // 
+	@Override
+	public int manageRequest(ReviewAuthDTO reviewAuthDTO) {
+
+		int result = 0;
+
+		try {
+			String sql = " update tbl_review_auth set is_permitted = ? where review_auth_id = ? ";
+
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, reviewAuthDTO.getIsPermitted());
+			pstmt.setInt(2, reviewAuthDTO.getReviewAuthId());
+
+			result = pstmt.executeUpdate(); // sql문 실행
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+
+		return result;
+
 	}
 
 }
